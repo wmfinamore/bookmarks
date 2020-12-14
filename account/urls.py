f0903c0f2ceb6549ddada08cfaa57e0390333f82
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-12-14 18:04:23 -0300

    Implemented the login/logout views and routes for the application

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import user_login
+from django.contrib.auth import views as auth_views
+from .views import user_login, dashboard
 
 
 urlpatterns = [
-    path('login/', user_login, name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', dashboard, name='dashboard'),
 ]
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'account',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'account',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
